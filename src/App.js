d10ad98898ce2a17266c3bbc77b301266762297d
Ly0nZ <charleymlyons@gmail.com>
2019-06-12 17:00:26 -0700

    Added API call functionality

--- src/App.js
+++ src/App.js
@@ -3,14 +3,30 @@ import Titles from "./components/Titles";
 import Form from "./components/Form";
 import Weather from "./components/Weather";
 
+const API_KEY = "31e231aaaedc37ea8ba0dba33af389fa";
+
 class App extends React.Component {
+  state = {
+    temperature: undefined,
+    city: undefined,
+    humidity: undefined,
+    descrition: undefined,
+    error: undefined
+  }
+  // Weather API call function 
+  getWeather = async (e) => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
+    const data = await api_call.json();
+    console.log(data);
+  }
   render() {
     return (
       <div>
         <Titles />
-
-        <Form />
-
+        <Form getWeather={this.getWeather}/>
         <Weather />
       </div>
     );
